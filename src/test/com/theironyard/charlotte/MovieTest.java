8947e368b11a7eeab341ce63e4cdf47032637cb1
Jennifer Chang <jennichang02@gmail.com>
2016-08-10 21:26:31 -0400

    updated assignment

---
+++ src/test/com/theironyard/charlotte/MovieTest.java
@@ -0,0 +1,22 @@
+package com.theironyard.charlotte;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by jenniferchang on 8/10/16.
+ */
+public class MovieTest {
+    @Test
+    public void saveData() throws Exception {
+
+
+    }
+
+    @Test
+    public void loadData() throws Exception {
+
+    }
+
+}
